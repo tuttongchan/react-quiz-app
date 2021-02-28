200d51e9845e5753252f64e6d6858b3d7ee65dbc
Tut Tongchantra <tuttongchan@Tuts-MacBook-Air.local>
2021-02-27 19:59:51 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const questions = [
+    {
+      questionText: "What is Digital's favorite brand?",
+      answerOptions: [
+        { answerText: 'Dries Van Noten', isCorrect: false },
+        { answerText: 'Maison Margiela', isCorrect: false },
+        { answerText: 'Cactus Jack', isCorrect: true },
+        { answerText: 'Jean Paul Gaultier', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Do we love these hoes?',
+      answerOptions: [
+        { answerText: 'No', isCorrect: false },
+        { answerText: 'Yes', isCorrect: false },
+        { answerText: 'Maybe', isCorrect: false },
+        { answerText: 'Never', isCorrect: true },
+      ],
+    },
+    {
+      questionText: 'Do you really feel fly in True Religon?',
+      answerOptions: [
+        { answerText: 'No', isCorrect: false },
+        { answerText: 'Yes', isCorrect: true },
+        { answerText: 'Maybe', isCorrect: false },
+        { answerText: 'Never', isCorrect: false },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [showScore, setShowScore] = useState(false)
+
+  const [score, setScore] = useState(0)
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if(isCorrect === true) {
+      // alert('this answer is correct!')
+      setScore(score + 1)
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
